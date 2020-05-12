c5cd845ad45dcc1e1a28289296fe517d618e46cd
SuperHaker <amrits3624@gmail.com>
2020-05-12 15:33:54 +0530

    Heroku setup

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side js loaded')
-
 const weatherForm = document.querySelector('form')
 const weatherText = document.querySelector('input')
 
@@ -10,7 +8,7 @@ weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
     forecastText.textContent=''
 
-    fetch('http://localhost:3000/weather?address=' + weatherText.value).then((response) => {
+    fetch('/weather?address=' + weatherText.value).then((response) => {
     response.json().then((data) => {
         if(data.error) {
             locationText.textContent = data.error
